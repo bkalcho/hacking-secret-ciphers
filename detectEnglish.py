334bec684faba73e8969a595939716142d807799
Bojan Kalicanin <kalcho@zoho.com>
2017-02-28 19:16:26 +0100

    detect english programmatically module

---
+++ detectEnglish.py
@@ -0,0 +1,46 @@
+# Author: Bojan G. Kalicanin
+# Date: 28-Feb-2017
+# Description: Module for detecting English language programmatically.
+
+upperletters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+letters_and_space = upperletters + upperletters.lower() + ' \t\n'
+
+def loadDictionary():
+    dictionaryFile = open('dictionary.txt')
+    englishWords = {}
+    for word in dictionaryFile.read().split('\n'):
+        englishWords[word] = None
+    dictionaryFile.close()
+    return englishWords
+
+english_words = loadDictionary()
+
+
+def getEnglishCount():
+    message = message.upper()
+    message = removeNonLetters(message)
+    possibleWords = message.split()
+    if possibleWords == []:
+        return 0.0 # no words at all
+
+    matches = 0
+    for word in possibleWords:
+        if word in english_words:
+            matches += 1
+    return float(matches) / len(possibleWord)
+
+
+def removeNonLetters(message):
+    lettersOnly = []
+    for symbol in message:
+        if symbol in letters_and_space:
+            lettersOnly.append(symbol)
+    return ''.join(lettersOnly)
+
+
+def isEnglish(message, wordPercentage=20, letterPercentage=85):
+    wordsMatch = getEnglishCount(message) * 100 >= wordPercentage
+    numLetters = len(removeNonLetters(message))
+    messageLettersPercentage = float(numLetters) / len(message) * 100
+    lettersMatch = messageLettersPercentage >= letterPercentage
+    return wordsMatch and lettersMatch
