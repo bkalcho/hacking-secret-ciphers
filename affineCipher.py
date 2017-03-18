d0be56188d9fc36822c0945192859c9664868494
Bojan Kalicanin <kalcho@zoho.com>
2017-03-18 15:31:44 +0100

    debugged affine cipher

--- affineCipher.py
+++ affineCipher.py
@@ -4,8 +4,7 @@
 
 import sys, paperclip, cryptomath, random
 
-symbols = """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]
-^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+symbols = """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
 
 def main():
     myMessage = """"A computer would deserve to be called intelligent if it
@@ -20,7 +19,7 @@ def main():
     print('Key: {0:d}'.format(myKey))
     print('{0:s}ed text:'.format(myMode.title()))
     print(translated)
-    pyperclip.copy(translated)
+    paperclip.copy(translated)
     print('Full {0:s}ed text copied to clipboard.'.format(myMode))
 
 
@@ -33,16 +32,16 @@ def getKeyParts(key):
 def checkKeys(keyA, keyB, mode):
     if keyA == 1 and mode == 'encrypt':
         sys.exit('The affine cipher becomes incredibly weak when key A is \
-            set to 1. Choose a different key.')
+set to 1. Choose a different key.')
     if keyB == 0 and mode == 'encrypt':
         sys.exit('The affine cipher becomes icredibly weak when key B is \
-            set to 0. Choose a different key.')
+set to 0. Choose a different key.')
     if keyA < 0 and keyB < 0 or keyB > len(symbols) - 1:
         sys.exit('Key A must be greater than 0 and Key B must be between 0 \
-            and {0:d}.'.format(len(symbols) - 1))
+and {0:d}.'.format(len(symbols) - 1))
     if cryptomath.gcd(keyA, len(symbols)) != 1:
         sys.exit('Key A ({0:d}) and the symbol set size ({1:d}) are not \
-            relatively prime. Choose a different key.'.format(keyA, len(symbols)))
+relatively prime. Choose a different key.'.format(keyA, len(symbols)))
 
 
 def encryptMessage(key, message):
@@ -53,7 +52,7 @@ def encryptMessage(key, message):
         if symbol in symbols:
             # encrypt this symbol
             symIndex = symbols.find(symbol)
-            cyphertext += symbols[(symIndex * keyA + keyB) % len(symbols)]
+            ciphertext += symbols[(symIndex * keyA + keyB) % len(symbols)]
         else:
             ciphertext += symbol # just append this symbol unencrypted
     return ciphertext
